7549064afc4339b6727dba338e3df31c7bc60c92
Wing Kei Leung <winky.leung@gmail.com>
2017-08-03 00:29:32 +0000

    Push logout button

--- express_server.js
+++ express_server.js
@@ -75,6 +75,11 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect('/urls');
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie("username")
+  res.redirect("/urls")
+});
+
 app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect('/urls');
